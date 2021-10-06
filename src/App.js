2ff4aac8bd29e8e28d64b8b20632a5e749e85510
Stephanie Opala <rispahopala@gmail.com>
2021-10-06 16:11:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Photo from "./components/Photo";
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
+  useEffect(() => {
+    const getItems = async () => {
+      try {
+        const response = await fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_API_KEY}&q=${query}&image_type=photo`);
+        if(response.ok){
+          const data = await response.json();
+          console.log(data);
+          setItems(data.hits);
+        } else {
+          throw new Error("Request failed!")
+        }
+      } catch(err){
+        console.log(err)
+      }
+    }
+    getItems();
+  }, [query])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col justify-center p-5">
+      <form onSubmit={handleSubmit} className="w-full mb-3 flex justify-center" >
+        <input type="text" 
+          placeholder="Search image" 
+          onChange={(e) => setSearch(e.target.value) }
+          value={search}
+          className="p-2 w-96 focus-within:border-gray-400"
+        />
+        <input type="submit" 
+          value="Search"
+          className="px-4 py-1 border-1 rounded-m cursor-pointer"
+        />
+      </form>
+      <div className="flex flex-wrap justify-center">
+        {items.map(item => {
+          return (<Photo
+            key={uuidv4()}
+            image={item.previewURL}
+            views={item.views}
+            downloads={item.downloads}
+            likes={item.likes}
+            tags={item.tags}
+            user={item.user}
+          />)
+        })}
+      </div>
     </div>
+
   );
 }
-
+  
 export default App;
---
+++ src/components/Photo.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Photo = ({image, views, downloads, likes, tags, user}) => {
+    return(
+        <div className="w-100 rounded overflow-hidden shadow-lg p-2 mb-10">
+            <img src={image} alt="profile" className="w-full h-40 object-contain"/>
+            <div className="px-6 py-4">
+                <div className=" font-bold text-purple-500 text-md mb-2">
+                    Photo by {user}
+                </div>
+                <ul>
+                    <li>
+                        <strong>Views: </strong>
+                        {views}
+                    </li>
+                    <li>
+                        <strong>Downloads: </strong>
+                        {downloads}
+                    </li>
+                    <li>
+                        <strong>Likes: </strong>
+                        {likes}
+                    </li>
+                </ul>
+            </div>
+            <div className="px-6 py-4">
+                <span className="inline-block bg-gray-200 text-gray-700 rounded-full
+                px-3 py-1 text-sm font-semibold mr-2">
+                    #tag1
+                </span>
+                <span className="inline-block bg-gray-200 text-gray-700 rounded-full
+                px-3 py-1 text-sm font-semibold mr-2">
+                    #tag2
+                </span>
+                <span className="inline-block bg-gray-200 text-gray-700 rounded-full
+                px-3 py-1 text-sm font-semibold mr-2">
+                    #tag3
+                </span>
+            </div>
+        </div>
+    )
+}
+export default Photo;
